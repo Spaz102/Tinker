3f89da8f4bb4b1adcbf46ae98f1dbdff43d426eb
Ambiwlans <udp.castellani@gmail.com>
2022-04-02 13:56:36 -0400

    minor - delete options.cs

--- Assets/Settings.cs
+++ Assets/Settings.cs
@@ -57,3 +57,26 @@ public class Settings
 		PlayerPrefs.Save();
 	}
 }
+
+
+
+
+/*using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour {
+	public Text headtext;
+	public Text bodytext;
+
+	void Start () {
+		this.gameObject.transform.localPosition = new Vector3(0f, -15f, 0f);
+		this.gameObject.SetActive(false);
+	}
+
+	void Update () {
+
+	}
+
+	
+}*/
